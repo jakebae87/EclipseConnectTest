b50d06ea101cdc6d07d2dc98578eae88d2cc1e32
jakebae <jeonghyun.bae@kakao.com>
2022-02-09 21:22:07 +0900

    gugudan

--- src/Test/gugudan.java
+++ src/Test/gugudan.java
@@ -3,15 +3,12 @@ package Test;
 public class gugudan {
 
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 2;
-		
-		if(a<b) {
-			System.out.println("correct");
-		}else {
-			System.out.println("try again");
+		for (int i = 2; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.print(i + "x" + j + "=" + i * j);
+			}
+			System.out.println();
 		}
 
 	}
-
 }
