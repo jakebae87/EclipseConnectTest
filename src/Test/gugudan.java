5b83aadf5d9740fdecf12cb02fec5f6df874ce09
jakebae <jeonghyun.bae@kakao.com>
2022-02-09 19:40:01 +0900

    This is second test

---
+++ src/Test/gugudan.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		
+		if(a<b) {
+			System.out.println("correct");
+		}else {
+			System.out.println("try again");
+		}
+
+	}
+
+}
